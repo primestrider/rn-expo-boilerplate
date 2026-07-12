828e288c01dc116b994ac411fe5642c17f48ce96
primestrider <theprimestrider@gmail.com>
2026-07-12 23:25:04 +0700

    feat: add script build android

--- configs/android.config.ts
+++ configs/android.config.ts
@@ -1,7 +1,18 @@
-import { ExpoConfig } from "expo/config";
+import type { ExpoConfig } from "expo/config";
+
+import packageJson from "../package.json";
+
+function getVersionCode(version: string): number {
+  const [major, minor, patch] = version.split(".").map(Number);
+
+  return major * 10000 + minor * 100 + patch;
+}
 
 export const androidConfig: ExpoConfig["android"] = {
   package: "com.primestrider.rnexpoboilerplate",
+
+  versionCode: getVersionCode(packageJson.version),
+
   adaptiveIcon: {
     backgroundColor: "#E6F4FE",
     foregroundImage: "./assets/images/android-icon-foreground.png",
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,29 +1,11 @@
-import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
 
 import { AppProvider } from "@/providers/AppProvider";
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [fontLoaded] = useFonts({
-    "PlusJakartaSans-Regular": require("@/assets/fonts/PlusJakartaSans-Regular.ttf"),
-    "PlusJakartaSans-Medium": require("@/assets/fonts/PlusJakartaSans-Medium.ttf"),
-    "PlusJakartaSans-SemiBold": require("@/assets/fonts/PlusJakartaSans-SemiBold.ttf"),
-    "PlusJakartaSans-Bold": require("@/assets/fonts/PlusJakartaSans-Bold.ttf"),
-    "PlusJakartaSans-ExtraBold": require("@/assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
-  });
-
-  useEffect(() => {
-    if (fontLoaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [fontLoaded]);
-
-  if (!fontLoaded) return null;
-
   return (
     <AppProvider>
       <Stack />
